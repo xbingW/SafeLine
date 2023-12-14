ac26edc62c5e0c5df759455d494a2527ad03ce41
lanlan <lan.lan@chaitin.com>
2023-12-14 14:31:19 +0800

    fix(website): add og:image meta

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -14,6 +14,7 @@ export default function MyApp({ Component, pageProps }: any) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="keywords" content="WAF,雷池,长亭,社区版,免费,开源,网站防护"></meta>
         <meta name="description" content="一款足够简单、好用、强大的免费WAF。基于业界领先的语义分析检测技术，保护你的网站不受黑客攻击。"></meta>
+        <meta property="og:image" content="https://waf-ce.chaitin.cn/safeline.png"></meta>
       </Head>
       <ThemeProvider>
         <NavBar />
